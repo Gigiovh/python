c756b2e7942899e4119cb783e560db2839aacd25
Giovanna <132226469+Gigiovh@users.noreply.github.com>
2023-05-28 15:07:38 -0300

    Add files via upload

---
+++ exercicio 5 lista.py
@@ -0,0 +1,7 @@
+#produto
+n1=float(input('Digite o valor em metros: '))
+n2=float(input('Digite o valor em metros: '))
+n3=float(input('Digite o valor em metros:'))
+
+a=(n1* n2 * n3)
+print('O valor do produto dos números {} ,{} ,{} é :{}'.format(n1, n2, n3,a))
