ee390bc3123add15a93f1e0586a1f4deeb665bdb
Giovanna Clemente <132226469+Gigiovh@users.noreply.github.com>
2026-01-06 11:39:46 -0300

    Update and rename lista do python/exercicio19.py to Exercicios/Estrutura de Condição/IF /imc.py
    refactor: move files to new folder

--- Exercicios/Estrutura de Condição/IF /imc.py
+++ Exercicios/Estrutura de Condição/IF /imc.py
@@ -1,3 +1,4 @@
+#peso
 n1=float(input('Digite a quanto você tem de altura:'))
 n2=float(input('Digite o seu peso:'))
 imc= n2 / (n1 ** 2)
@@ -10,4 +11,5 @@ elif imc> 25 and imc <30:
 elif imc>30 and imc <40:
     print('Você está obeso')
 else:
+
     print('Você está com obesidade mórbita')
