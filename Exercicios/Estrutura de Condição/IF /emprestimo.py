8521787dc6b3150edf631d75ba34a3cad4df41d6
Giovanna Clemente <132226469+Gigiovh@users.noreply.github.com>
2026-01-06 11:35:21 -0300

    Update and rename lista do python/exercicio14.py to Exercicios/Estrutura de Condição/IF /emprestimo.py
    refactor: move files to new folder

--- Exercicios/Estrutura de Condição/IF /emprestimo.py
+++ Exercicios/Estrutura de Condição/IF /emprestimo.py
@@ -1,3 +1,4 @@
+#emprestimo
 n1=float(input('Digite o  valor da casa :'))
 n2= float(input('Digite em quantos anos você irá pagar a casa:'))
 n3= float(input('Digite o valor do seu salário:'))
@@ -6,4 +7,5 @@ p= n1/n3
 if p<= n:
     print('Você pode fazer o empréstimo')
 else:
+
     print('Você não pode fazer o empréstimo')
