3227b55947318711424097d1e9316f142531cb39
Giovanna Clemente <132226469+Gigiovh@users.noreply.github.com>
2026-01-06 11:45:24 -0300

    Update and rename exercicio 2 lista.py to Exercicios/Operadores Aritméticos/multiplicação.py
    refactor: move files to new folder

--- exercicio 2 lista.py
+++
@@ -1,7 +0,0 @@
-#Dobro, Triplo e Raiz
-import math
-n1=float(input('digite um número:'))
-a=(n1*2 )
-s=(n1*3)
-r=math.sqrt( n1)
-print('O dobro é igual á {}, \n O triplo é igual á {} \n a raiz quadrada de {}  é igual á {}'.format(a,s,n1,r))
