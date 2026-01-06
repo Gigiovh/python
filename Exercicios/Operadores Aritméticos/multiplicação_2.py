5f2eec36ac0974fd85c6250288b3f0c33602f9b1
Giovanna Clemente <132226469+Gigiovh@users.noreply.github.com>
2026-01-06 11:51:41 -0300

    Update and rename exercicio 5 lista.py to  Exercicios/Operadores Aritméticos/multiplicação_2.py
    refactor: move files to new folder

--- Exercicios/Operadores Aritméticos/multiplicação_2.py
+++ Exercicios/Operadores Aritméticos/multiplicação_2.py
@@ -1,7 +1,8 @@
-#produto
+#multiplicação
 n1=float(input('Digite o valor em metros: '))
 n2=float(input('Digite o valor em metros: '))
 n3=float(input('Digite o valor em metros:'))
 
 a=(n1* n2 * n3)
+
 print('O valor do produto dos números {} ,{} ,{} é :{}'.format(n1, n2, n3,a))
